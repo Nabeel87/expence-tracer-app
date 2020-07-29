7e2c9b073c3cf0ca7d9f5fea5764c71df8ec5413
Nabeel87 <nabilsabir786@gmail.com>
2020-07-29 11:14:45 +0500

    Final Update

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div>
-            <h3>Thanks to PIAIC</h3>
+            <h3>Thanks to PIAIC. (Nabil)</h3>
         </div>
     )
 }
